65cc63efcac0a5f2532e9196b1a7ec236128fa0e
Operel <oriperelman@gmail.com>
2019-05-15 06:30:09 +0300

    refactoring

--- columns_list.py
+++ columns_list.py
@@ -4,11 +4,11 @@ class Columns():
         self.lis = lis
         self.slice_len = slice_len
 
-    # slice the list into n sized chuncks
     def s_list(self):
+        ''' slice the list into n sized chuncks '''
         for i in range(0, len(self.lis), self.slice_len):
             yield self.lis[i:i + self.slice_len]
 
-    # create columns sublists
     def columns(self):
+        ''' create columns sublists '''
         return list(zip(*self.s_list()))
